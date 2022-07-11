1d73c29bc19c4d4b581b97ad39891c150b74bbff
purcymarte <purcymarte@gmail.com>
2022-07-11 17:42:26 +0200

    chore: add dummy header components

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,7 +1,15 @@
 interface HeadingProps {
   title: string;
+  content: string;
 }
 
 export default function Heading({ title }: HeadingProps) {
     return (<h1>{title}</h1>);
+}
+
+export const Heading1 = ({content}: HeadingProps) => {
+  return <h1>{content}</h1>
+}
+export const Heading2 = ({content}: HeadingProps) => {
+  return <h2>{content}</h2>
 }
