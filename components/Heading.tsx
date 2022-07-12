54a2b634772dd7cf98b20519f29a5b8d1ce25ad3
552020 <stefanolombardo@posteo.de>
2022-07-12 12:43:51 +0200

    grommet added to the heading component

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,10 +1,28 @@
+import { Grommet, Heading } from 'grommet'
+
 interface HeadingProps {
-  content: string;
+  content: string
 }
 
-export const Heading1 = ({content}: HeadingProps) => {
-  return <h1>{content}</h1>
+export const Heading1 = ({ content }: HeadingProps) => {
+  return <Heading level="1">{content}</Heading>
 }
-export const Heading2 = ({content}: HeadingProps) => {
-  return <h2>{content}</h2>
+export const Heading2 = ({ content }: HeadingProps) => {
+  return <Heading level="2">{content}</Heading>
+}
+
+export const Heading3 = ({ content }: HeadingProps) => {
+  return <Heading level="3">{content}</Heading>
+}
+
+export const Heading4 = ({ content }: HeadingProps) => {
+  return <Heading level="4">{content}</Heading>
+}
+
+export const Heading5 = ({ content }: HeadingProps) => {
+  return <Heading level="5">{content}</Heading>
+}
+
+export const Heading6 = ({ content }: HeadingProps) => {
+  return <Heading level="6">{content}</Heading>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,77 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Box, Grommet } from "grommet";
+import { Box, Grommet } from 'grommet'
 
 import Code from '../components/Code'
-import { Heading1, Heading2 } from '../components/Heading'
+import {
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  Heading5,
+  Heading6,
+} from '../components/Heading'
 
 // map available components
-const components = { Heading1, Heading2, Code };
+const components = {
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  Heading5,
+  Heading6,
+  Code,
+}
 
 // dummy json
 const json = [
   {
-    name: "Heading1",
-    content: "This is some heading content for heading 1",
+    name: 'Heading1',
+    content: 'This is some heading content for heading 1',
   },
   {
-    name: "Heading2",
-    content: "This is some other content for heading 2",
+    name: 'Heading2',
+    content: 'This is some other content for heading 2',
   },
   {
-    name: "Code",
-    content: "const test = 123",
+    name: 'Heading3',
+    content: 'This is some other content for heading 3',
+  },
+  {
+    name: 'Heading4',
+    content: 'This is some other content for heading 4',
+  },
+  {
+    name: 'Heading5',
+    content: 'This is some other content for heading 5',
+  },
+  {
+    name: 'Heading6',
+    content: 'This is some other content for heading 6',
+  },
+  {
+    name: 'Code',
+    content: 'const test = 123',
     props: {
-      language: "javascript"
-    }
-  }
-];
+      language: 'javascript',
+    },
+  },
+]
 
 // dynamic component
-const DynamicComponent = (component:{ name: string; content: string; props?: {language?:string}}, i:number) => {
-  const Component = components[component.name as keyof typeof components];
+const DynamicComponent = (
+  component: { name: string; content: string; props?: { language?: string } },
+  i: number,
+) => {
+  const Component = components[component.name as keyof typeof components]
   const _props = component.props ? component.props : {}
   if (!Component) {
     console.error(`component ${component.name} was not found in component map`)
-    return <></>;
+    return <></>
   }
-  return <Component key={i} content={component.content} props={_props}/>;
-};
+  return <Component key={i} content={component.content} props={_props} />
+}
 
 const AppBar = (props: any) => (
   <Box
@@ -46,41 +80,40 @@ const AppBar = (props: any) => (
     align="center"
     justify="between"
     background="brand"
-    pad={{ left: "medium", right: "small", vertical: "small" }}
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
     elevation="medium"
-    style={{ zIndex: "1" }}
+    style={{ zIndex: '1' }}
     {...props}
   />
-);
+)
 
 const Home: NextPage = () => {
   const theme = {
     global: {
       colors: {
-        brand: "#228BE6",
+        brand: '#228BE6',
       },
       font: {
-        family: "Roboto",
-        size: "18px",
-        height: "20px",
+        family: 'Roboto',
+        size: '18px',
+        height: '20px',
       },
     },
-  };
+  }
   return (
     <div>
       <Head>
         <title>Formartion.tools</title>
       </Head>
       <Grommet theme={theme}>
-        <main>
-        </main>
+        <main></main>
         <AppBar>Hello Grommet!</AppBar>
-        <h1>Welcome to Formation!</h1>
+        <h1>Welcome to Formation.tools!</h1>
         {/* iterate over json, build right component */}
-        <div>{json.map((component,i) => DynamicComponent(component,i))}</div>
+        <div>{json.map((component, i) => DynamicComponent(component, i))}</div>
       </Grommet>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
