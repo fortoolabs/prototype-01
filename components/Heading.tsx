40615737ec9398e239cd16966c065ae94906b99c
552020 <stefanolombardo@posteo.de>
2022-07-12 14:30:46 +0200

    first try to pass the level as a property of a component

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -2,27 +2,9 @@ import { Grommet, Heading } from 'grommet'
 
 interface HeadingProps {
   content: string
+  props: { level: string }
 }
 
-export const Heading1 = ({ content }: HeadingProps) => {
-  return <Heading level="1">{content}</Heading>
-}
-export const Heading2 = ({ content }: HeadingProps) => {
-  return <Heading level="2">{content}</Heading>
-}
-
-export const Heading3 = ({ content }: HeadingProps) => {
-  return <Heading level="3">{content}</Heading>
-}
-
-export const Heading4 = ({ content }: HeadingProps) => {
-  return <Heading level="4">{content}</Heading>
-}
-
-export const Heading5 = ({ content }: HeadingProps) => {
-  return <Heading level="5">{content}</Heading>
-}
-
-export const Heading6 = ({ content }: HeadingProps) => {
-  return <Heading level="6">{content}</Heading>
+export const HeadingGrommet = ({ content, props }: HeadingProps) => {
+  return <Heading level={props.level}>{content}</Heading>
 }
