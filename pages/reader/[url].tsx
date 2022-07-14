c3dac9bdc8802fd025ed860e20ac6f04a439bcbc
purcymarte <purcymarte@gmail.com>
2022-07-14 15:42:02 +0200

    wip: combine board and list view

--- pages/reader/[url].tsx
+++ pages/reader/[url].tsx
@@ -1,19 +1,16 @@
 import { useRouter } from 'next/router'
 import { useState } from 'react'
-import base64url from "base64url";
+import base64url from 'base64url'
 
+import Heading from '../../components/Heading'
 import Board from '../../components/Board'
-import { AppContainer, Main, MainContent } from '../../components/View'
+import { AppContainer, Main, MainContent, Row } from '../../components/View'
 
 interface ReaderProps {
   url: string
 }
 
-const Reader = ({ url }: ReaderProps) => {
-  const [ boardView, setBoardView ] = useState(true)
-
-  if (boardView) return <Board url={url}/>
-
+const _ListView = ({ url }: ReaderProps) => {
   return (
     <AppContainer>
       <Main>
@@ -22,6 +19,21 @@ const Reader = ({ url }: ReaderProps) => {
         </MainContent>
       </Main>
     </AppContainer>
+  )
+}
+
+const Reader = ({ url }: ReaderProps) => {
+  const [boardView, setBoardView] = useState(true)
+
+  return (
+    <>
+      <Row align="center" gap="medium" justify="end" pad="medium">
+        <Heading level={3} title={base64url.decode(url)}/>
+        <span onClick={() => setBoardView(false)}>list</span>
+        <span onClick={() => setBoardView(true)}>board</span>
+      </Row>
+      {!!boardView ? <Board url={url} /> : <_ListView url={url} />}
+    </>
   )
 }
 
