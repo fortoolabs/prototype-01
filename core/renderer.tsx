265f43407f195365fd82ba21aef06514d478f3a9
David Asabina <vid@bina.me>
2022-07-16 08:12:28 +0000

    fix: provide every pages/index items a unique key
    Keep in mind that this is just safe as long as the list doesn't change (no
    dynamic data retrieval, no filter, etc.).
    
    https://robinpokorny.medium.com/index-as-a-key-is-an-anti-pattern-e0349aece318

--- core/renderer.tsx
+++ core/renderer.tsx
@@ -53,21 +53,25 @@ function assertExhaustive(
 }
 
 export default function generateComponent(el: DocumentElement, idx: number) {
+  // WARN: Using index as key, which is okay for static listings
+  // https://robinpokorny.medium.com/index-as-a-key-is-an-anti-pattern-e0349aece318
+  const i = idx.toString()
+
   // TODO: De-couple component type from storage type
   switch (el.name) {
     case 'Heading':
       // TODO: Implement fallback when level>6
-      return <Heading title={el.data.title} level={el.data.level} />
+      return <Heading key={i} title={el.data.title} level={el.data.level} />
     case 'Code':
-      return <Code language={el.data.language} source={el.data.source} />
+      return <Code key={i} language={el.data.language} source={el.data.source} />
     case 'Paragraph':
-      return <Paragraph>{el.data.children}</Paragraph>
+      return <Paragraph key={i}>{el.data.children}</Paragraph>
     case 'Date':
-      return <Date timestamp={el.data.timestamp} />
+      return <Date key={i} timestamp={el.data.timestamp} />
     case 'FallbackInline':
-      return <FallbackInline content={el.data.content} />
+      return <FallbackInline key={i} content={el.data.content} />
     case 'FallbackBlock':
-      return <FallbackBlock>{el.data.children}</FallbackBlock>
+      return <FallbackBlock key={i}>{el.data.children}</FallbackBlock>
     default:
       return assertExhaustive(el)
   }
