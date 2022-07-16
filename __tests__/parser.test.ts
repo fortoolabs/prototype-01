68c7bc9c4383ccc680d0719e7b337d57fb0e3b14
David Asabina <vid@bina.me>
2022-07-16 22:30:15 +0000

    chore: stub test for paragraph-to-string conversion

--- __tests__/parser.test.ts
+++ __tests__/parser.test.ts
@@ -23,6 +23,12 @@ test('parses', () => {
   expect(proc('__fixtures__/Roadmap.org')).toEqual({
     todoStates: ['TODO', 'IDEA', 'SCOPE', 'INSKETCH', 'INDEV', 'DONE(d)'],
     title: 'Product Roadmap',
-    content: [],
+    content: [
+      {
+        type: 'Paragraph',
+        content:
+          '⚠️ All points in this roadmap are listed in their relative order of importance. In case points get reordered in this document, it should be interpreted as a change in prioritization. Points are ordered by highest priority first.\n',
+      },
+    ],
   })
 })
