dce6957dcc41ce5a5b8524f3ea98f03b1e8785e2
David Asabina <vid@bina.me>
2022-07-16 22:26:03 +0000

    feat: stub rough paragraph parsing

--- core/parser.ts
+++ core/parser.ts
@@ -55,6 +55,47 @@ function reduceKeyword(acc: FDocument, x: Keyword): FDocument {
   }
 }
 
+function mapParagraphContent(x: ObjectType): string {
+  // TODO: Restructure to a list of non-string members
+  // A lot of information is lost in just reducing this to strings
+  switch (x.type) {
+    case 'link':
+      return x.rawLink
+    case 'bold':
+    case 'italic':
+    case 'strike-through':
+    case 'underline':
+    case 'superscript':
+    case 'subscript':
+      return x.children.map(mapParagraphContent).join('')
+    case 'code':
+    case 'verbatim':
+    case 'text':
+      return x.value
+    case 'timestamp':
+      return x.rawValue
+    case 'footnote-reference':
+      return x.label
+    case 'latex-fragment':
+      return x.value
+    case 'entity':
+      return x.name
+    case 'table-cell':
+      return 'CELL'
+    default:
+      //return assertExhaustive(x)
+      return ''
+  }
+}
+
+function reduceParagraph(acc: FDocument, x: Paragraph): FDocument {
+  const paragraphText = x.children.map(mapParagraphContent, []).join('')
+  return {
+    ...acc,
+    content: [...acc.content, { type: 'Paragraph', content: paragraphText }],
+  }
+}
+
 // https://github.com/rasendubi/uniorg/blob/master/packages/uniorg/src/index.ts#L56
 // Link -> RecursiveObject
 //function extractObjectType(x: ObjectType): string {
@@ -102,7 +143,7 @@ function r(acc: FDocument, node: OrgNode, idx: number): FDocument {
     case 'keyword':
       return reduceKeyword(acc, node)
     case 'paragraph':
-      return acc
+      return reduceParagraph(acc, node)
     //return { ...acc, children: reduceParagraph(acc.content, node) }
     //case 'section':
     //    const x = (node as Section).children
