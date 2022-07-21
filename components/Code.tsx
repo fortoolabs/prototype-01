4b4079f352cb86705e568225a8481df0c2d11c81
purcymarte <purcymarte@gmail.com>
2022-07-21 18:20:39 +0200

    chore: add dark mode for code element

--- components/Code.tsx
+++ components/Code.tsx
@@ -1,17 +1,28 @@
+import type { FC } from 'react'
+import { useContext } from 'react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { Box } from 'grommet'
+import { oneDark, oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { Box, ThemeContext } from 'grommet'
 
 export type CodeProps = {
   language: string
   source: string
 }
 
-export default function Code({ language, source }: CodeProps) {
+const Code: FC<CodeProps> = props => {
+  const theme = useContext(ThemeContext)
   return (
     <Box alignSelf="stretch">
-      <SyntaxHighlighter tabIndex="0" wrapLongLines language={language}>
-        {source}
+      <SyntaxHighlighter
+        style={theme.dark ? oneDark : oneLight}
+        customStyletabIndex="0"
+        wrapLongLines
+        language={props.language}
+      >
+        {props.source}
       </SyntaxHighlighter>
     </Box>
   )
 }
+
+export default Code
