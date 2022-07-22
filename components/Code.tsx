2cc0bf969d168ee9f80c2f05a2c300b5e24acc90
purcymarte <purcymarte@gmail.com>
2022-07-22 15:10:46 +0200

    fix: use commonjs module for react syntax highlighter to fix unexpected export error

--- components/Code.tsx
+++ components/Code.tsx
@@ -1,7 +1,7 @@
 import type { FC } from 'react'
 import { useContext } from 'react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { oneDark, oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { oneDark, oneLight } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import { Box, ThemeContext } from 'grommet'
 
 export type CodeProps = {
