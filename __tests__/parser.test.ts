c4924b53140ad02ac6b87c896a1be7768a7093de
David Asabina <vid@bina.me>
2022-07-30 13:41:57 +0000

    chore: refactor tests and test headline tag extraction

--- __tests__/parser.test.ts
+++ __tests__/parser.test.ts
@@ -24,72 +24,81 @@ function readFixture(file: string, isDebugDumpEnabled: boolean): FDocument {
   return data
 }
 
-describe('parse', () => {
-  it('empty string to return empty document', () => {
-    expect(parse('')).toEqual(emptyDocument)
+it('empty string returns empty document', () => {
+  expect(parse('')).toEqual(emptyDocument)
+})
+
+it('single word returns document with single text entry', () => {
+  expect(parse('word')).toEqual({
+    content: [{ content: [{ content: 'word', type: 't' }], type: 'p' }],
+    todoStates: [],
+  })
+})
+
+describe('headline', () => {
+  it('has correct text', () => {
+    expect(parse('* How are you?').content[0].content).toEqual([
+      { type: 't', content: 'How are you?' },
+    ])
   })
 
-  it('single word to return document with single text entry', () => {
-    expect(parse('word')).toEqual({
-      content: [{ content: [{ content: 'word', type: 't' }], type: 'p' }],
-      todoStates: [],
-    })
+  it('without text has empty content', () => {
+    expect(parse('* ').content[0].content).toEqual([])
   })
 
-  describe('single 1st level headline', () => {
-    const data = parse('* How are you?')
-
-    it('returns list of a single text element', () => {
-      expect(data.content[0].content).toEqual([
-        { type: 't', content: 'How are you?' },
-      ])
-    })
-
-    it('has zero tags', () => {
-      expect(data.content[0].tags).toHaveLength(0)
-    })
-
-    it('has level 1', () => {
-      expect(data.content[0]).toHaveProperty('level', 1)
-    })
+  it('extracts tags', () => {
+    expect(parse('* Design share button').content[0].tags).toHaveLength(0)
+    expect(parse('* Design share button :ui:').content[0].tags).toHaveLength(1)
+    expect(
+      parse('* Design paraphrasing feature :ui:ML:NLP:').content[0].tags,
+    ).toHaveLength(3)
   })
 
-  describe('Roadmap.org', () => {
-    const ast = readFixture('Roadmap.org', isDebugDumpEnabled)
+  it('extracts the level', () => {
+    expect(parse('* How are you?').content[0]).toHaveProperty('level', 1)
+    expect(parse('**** How are you?').content[0]).toHaveProperty('level', 4)
+    expect(parse('******************* How are you?').content[0]).toHaveProperty(
+      'level',
+      19,
+    )
+  })
+})
 
-    it('to return all todo states', () => {
-      expect(ast.todoStates).toEqual([
-        'TODO',
-        'IDEA',
-        'SCOPE',
-        'INSKETCH',
-        'INDEV',
-        'DONE(d)',
-      ])
+describe('Roadmap.org', () => {
+  const ast = readFixture('Roadmap.org', isDebugDumpEnabled)
+
+  it('to return all todo states', () => {
+    expect(ast.todoStates).toEqual([
+      'TODO',
+      'IDEA',
+      'SCOPE',
+      'INSKETCH',
+      'INDEV',
+      'DONE(d)',
+    ])
+  })
+
+  it('to return the title', () => {
+    expect(ast.title).toEqual('Product Roadmap')
+  })
+
+  describe('top paragraph', () => {
+    const top = ast.content[0]
+
+    it('has matching type', () => {
+      expect(top).toHaveProperty('type', 'p')
     })
 
-    it('to return the title', () => {
-      expect(ast.title).toEqual('Product Roadmap')
-    })
-
-    describe('top paragraph', () => {
-      const top = ast.content[0]
-
-      it('has matching type', () => {
-        expect(top).toHaveProperty('type', 'p')
-      })
-
-      it('wraps a single text in a paragraph object', () => {
-        expect(top).toEqual({
-          type: 'p',
-          content: [
-            {
-              type: 't',
-              content:
-                '⚠️ All points in this roadmap are listed in their relative order of importance. In case points get reordered in this document, it should be interpreted as a change in prioritization. Points are ordered by highest priority first.\n',
-            },
-          ],
-        })
+    it('wraps a single text in a paragraph object', () => {
+      expect(top).toEqual({
+        type: 'p',
+        content: [
+          {
+            type: 't',
+            content:
+              '⚠️ All points in this roadmap are listed in their relative order of importance. In case points get reordered in this document, it should be interpreted as a change in prioritization. Points are ordered by highest priority first.\n',
+          },
+        ],
       })
     })
   })
