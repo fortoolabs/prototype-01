b1b9acd604eb207223844cb1ec928fdb654fe43a
David Asabina <vid@bina.me>
2022-08-05 15:46:26 +0000

    feat: stub parsing real raw text online

--- pages/reader/[url].tsx
+++ pages/reader/[url].tsx
@@ -1,6 +1,9 @@
 import { useRouter } from 'next/router'
+import { GetServerSideProps } from 'next'
 import { useState } from 'react'
 import base64url from 'base64url'
+
+import parse, { FDocument } from '../../core/parser'
 
 import Heading from '../../components/Heading'
 import Board from '../../components/Board'
@@ -9,15 +12,20 @@ import { AppContainer, Main, MainContent, Row } from '../../components/View'
 
 interface ReaderProps {
   url: string
+  doc: FDocument | null
+  isLoading: boolean
+  isError: Error | null
 }
 
-const Reader = ({ url }: ReaderProps) => {
+const fetcher = (url: string) => fetch(url).then((r) => r.text())
+
+const Reader = ({ url, doc }: ReaderProps) => {
   const [boardView, setBoardView] = useState(false)
 
   return (
     <>
       <Row align="center" gap="medium" justify="end" pad="medium">
-        <Heading level={3} title={base64url.decode(url)} />
+        <Heading level={3} title={url} />
         <span onClick={() => setBoardView(false)}>list</span>
         <span onClick={() => setBoardView(true)}>board</span>
       </Row>
@@ -27,13 +35,46 @@ const Reader = ({ url }: ReaderProps) => {
 }
 
 // This gets called on every request
-export async function getServerSideProps(context: any) {
-  const url = context.query.url
+const getServerSideProps: GetServerSideProps = async (
+  context,
+): Promise<{ props: ReaderProps }> => {
+  if (context.query.url === undefined) {
+    // TODO: redirect back to URL input
+    return {
+      props: {
+        url: 'https://dummy.example.com',
+        doc: parse(''),
+        isLoading: false,
+        isError: new Error('Invalid URL'),
+      },
+    }
+  }
 
-  // TODO: parse .org
+  const id = (id: string | string[]) => id
+  const concat = (acc: string[], x: string) => acc.concat(x)
+  //const x = [context.query.url].flatMap(id).reduce(concat, '')
+  // TODO: Remove hardcoded url and get line above to work
+  const x =
+    'https://gitlab.com/formation.tools/intel/product-vision/-/raw/main/Roadmap.org'
+  const url = base64url.decode(x)
+
+  const props: ReaderProps = await fetcher(url)
+    .then(parse)
+    .then((ast) => ({
+      url,
+      doc: ast,
+      isLoading: false,
+      isError: null,
+    }))
+    .catch((err) => ({
+      url,
+      doc: parse(''),
+      isLoading: false,
+      isError: err,
+    }))
 
   // Pass data to the page via props
-  return { props: { url: url } }
+  return { props }
 }
 
 export default Reader
