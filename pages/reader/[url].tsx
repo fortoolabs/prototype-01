e3edaa795f3918eb17d571c66ca9ce2f35803ecf
David Asabina <vid@bina.me>
2022-08-06 17:54:37 +0000

    chore: set server props are SWR fallback

--- pages/reader/[url].tsx
+++ pages/reader/[url].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { GetServerSideProps } from 'next'
+import { NextPage, GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import { useState } from 'react'
 import useSWR from 'swr'
 import base64url from 'base64url'
@@ -11,11 +11,10 @@ import Board from '../../components/Board'
 import List from '../../components/List'
 import { AppContainer, Main, MainContent, Row } from '../../components/View'
 
-interface ReaderProps {
-  url: string
-  doc: FDocument | undefined
-  isLoading: boolean
-  isFailing: Error | undefined
+type ReaderProps = {
+  url?: string
+  doc?: FDocument
+  isFailing: boolean
 }
 
 const fetcher = (url: string) =>
@@ -23,31 +22,48 @@ const fetcher = (url: string) =>
     .then((r) => r.text())
     .then((t) => parse(t))
 
-const useDoc = (url: string): ReaderProps => {
-  const { data, error, isValidating } = useSWR(url, fetcher)
+const useDoc = (
+  url: string,
+  { doc }: ReaderProps,
+): [ReaderProps, boolean, Error | undefined] => {
+  const { data, error, isValidating } = useSWR(url, fetcher, {
+    fallbackData: doc,
+  })
 
-  return {
-    url,
-    doc: data,
-    isLoading: isValidating,
-    isFailing: error,
-  }
+  return [
+    {
+      url,
+      doc: data,
+      isFailing: error ? true : false,
+    },
+    isValidating,
+    error,
+  ]
 }
 
-const Reader = (init: ReaderProps) => {
-  const { url, doc, isLoading, isFailing }: ReaderProps = useDoc(init.url)
+const Reader: NextPage<ReaderProps> = (props) => {
+  const [{ url, doc, isFailing }, isLoading, error] = useDoc(
+    props.url || '',
+    props,
+  )
+  console.log('props')
+  console.log(props)
   const [boardView, setBoardView] = useState(false)
 
   return (
     <>
       <Row align="center" gap="medium" justify="end" pad="medium">
-        <Heading level={3} title={url} />
         <pre>
+          🤔
+          {url}
           {isLoading ? '⏳' : ''}
           {isFailing ? '💥' : ''}
         </pre>
         <span onClick={() => setBoardView(false)}>list</span>
         <span onClick={() => setBoardView(true)}>board</span>
+      </Row>
+      <Row align="center" gap="medium" justify="end" pad="medium">
+        <pre>{JSON.stringify(props)}</pre>
       </Row>
       {boardView ? <Board /> : <List />}
     </>
@@ -55,41 +71,36 @@ const Reader = (init: ReaderProps) => {
 }
 
 // This gets called on every request
-const getServerSideProps: GetServerSideProps = async (
+// https://nextjs.org/docs/api-reference/data-fetching/get-server-side-props
+export const getServerSideProps: GetServerSideProps = async (
   context,
-): Promise<{ props: ReaderProps }> => {
+): Promise<{
+  props: ReaderProps
+}> => {
   if (context.query.url === undefined) {
     // TODO: redirect back to URL input
     return {
       props: {
-        url: 'https://dummy.example.com',
-        doc: parse(''),
-        isLoading: false,
-        isFailing: new Error('Invalid URL'),
+        isFailing: true,
       },
     }
   }
 
   const id = (id: string | string[]) => id
-  const concat = (acc: string[], x: string) => acc.concat(x)
-  //const x = [context.query.url].flatMap(id).reduce(concat, '')
-  // TODO: Remove hardcoded url and get line above to work
-  const x =
-    'https://gitlab.com/formation.tools/intel/product-vision/-/raw/main/Roadmap.org'
+  const concat = (acc: string, x: string) => acc.concat(x)
+  const x = [context.query.url].flatMap(id).reduce(concat, '')
   const url = base64url.decode(x)
 
   const props: ReaderProps = await fetcher(url)
     .then((ast) => ({
       url,
       doc: ast,
-      isLoading: false,
-      isFailing: undefined,
+      isFailing: false,
     }))
     .catch((err) => ({
       url,
       doc: parse(''),
-      isLoading: false,
-      isFailing: err,
+      isFailing: true,
     }))
 
   // Pass data to the page via props
