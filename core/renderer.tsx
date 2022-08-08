f4eddf48e27c4469b7bb29408fbe76b2d1e6d8ec
David Asabina <vid@bina.me>
2022-08-08 20:45:44 +0000

    chore: stub renderElementt and renderObject

--- core/renderer.tsx
+++ core/renderer.tsx
@@ -1,4 +1,5 @@
-import { FDocument, FHeading } from './types'
+import { Fragment } from 'react'
+import { FDocument, FHeading, FElementType, FObjectType } from './types'
 
 import Code, { CodeProps } from '../components/Code'
 import Heading, { HeadingProps } from '../components/Heading'
@@ -57,6 +58,75 @@ function assertExhaustive(
 // TODO: Make this the default export
 export function render(doc: FDocument): JSX.Element {
   return <span>nope</span>
+}
+
+export function renderElement(el: FElementType, i: number): JSX.Element[] {
+  switch (el.type) {
+    case 'h':
+      switch (el.level) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+          return [
+            <Heading
+              key={i}
+              title={el.content.flatMap(renderObject)}
+              level={el.level}
+            />,
+          ]
+        default:
+          // FIXME: Implement a fallback case
+          return []
+      }
+    case 'p':
+      return [<Paragraph key={i}>{el.content.flatMap(renderObject)}</Paragraph>]
+    default:
+      assertExhaustive(el)
+  }
+}
+
+export function renderObject(el: FObjectType, i: number): JSX.Element[] {
+  switch (el.type) {
+    case 'a':
+      return [<a href={el.target}>{el.content.flatMap(renderObject)}</a>]
+    case 'b':
+      return [<b>{el.content.flatMap(renderObject)}</b>]
+    case 'i':
+      return [<i>{el.content.flatMap(renderObject)}</i>]
+    case 'c':
+      return [<code>{el.content}</code>]
+    case 'v':
+      return [<code>{el.content}</code>]
+    case '+':
+      return [<s>{el.content.flatMap(renderObject)}</s>]
+    case 'u':
+      return [<u>{el.content.flatMap(renderObject)}</u>]
+    case '^':
+      return [<sup>{el.content.flatMap(renderObject)}</sup>]
+    case '_':
+      return [<sub>{el.content.flatMap(renderObject)}</sub>]
+    case 't':
+      return [<Fragment>{el.content}</Fragment>]
+    case 'Z':
+      // TODO: Pass along datetime
+      return [<time dateTime="2015-10-21">{el.content}</time>]
+    case 'f':
+      // TODO: Implement footnote
+      return [<em>{el.content.flatMap(renderObject)}</em>]
+    case 'X':
+      // TODO: Display LaTeX (using MathJaX or something server-side-rendered for better perf)
+      return [<code>{el.content}</code>]
+    case '?':
+      // TODO: Decide on and implement fallback-strategy for entity types
+      return [<code>{el.content}</code>]
+    case 'C':
+      return [<td>{el.content.flatMap(renderObject)}</td>]
+    default:
+      assertExhaustive(el)
+  }
 }
 
 export default function generateComponent(el: DocumentElement, idx: number) {
