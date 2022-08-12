3409aac6137412007e2dc2224c83a705c0edb091
David Asabina <vid@bina.me>
2022-08-12 10:48:30 +0000

    chore: name Linear + flatMap renderElement + make serif optional

--- components/Linear/index.tsx
+++ components/Linear/index.tsx
@@ -9,10 +9,10 @@ export type LinearProps = {
   // TODO: Revert to make this mandatory, optional doc is a hack to move this forward
   doc?: FDocument
   // TODO: Remove in preference of theming (as outlined below)
-  serif: boolean
+  serif?: boolean
 }
 
-export default function LinearView({ serif, doc }: LinearProps) {
+export default function Linear({ serif, doc }: LinearProps) {
   // TODO: Use theming context/provider for this or define own
   // https://reactician.com/articles/sharing-state-between-nextjs-page-navigations-using-react-contexts
   if (doc === undefined) {
@@ -24,7 +24,7 @@ export default function LinearView({ serif, doc }: LinearProps) {
       <Row gap="small" pad="small" justify="start">
         <Main style={{ fontFamily: serif ? 'Times' : 'inherit' }}>
           {/* iterate over json, build right component */}
-          {content.map((el, i) => renderElement(el, i))}
+          {content.flatMap((el, i) => renderElement(el, i))}
         </Main>
       </Row>
     )
