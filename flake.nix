19f5b9d483b9efd5b7b4409994830a56a751fae2
David Asabina <vid@bina.me>
2022-08-20 21:58:13 +0000

    chore: update flake nixpkgs input to unstable
    to meet npm engine constraint

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   nixConfig.bash-prompt = "ft > ";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
