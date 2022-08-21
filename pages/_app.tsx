cd045a13e63386d92de9b3acf9e3864495f02643
David Asabina <vid@bina.me>
2022-08-21 14:58:07 +0000

    chore: migrate _app to Tailwind + stub dark mode support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,29 @@
 import type { AppProps } from 'next/app'
-import { Grommet } from 'grommet'
+
 import { useState } from 'react'
 
-import { Col, Row } from 'components/View'
-import { theme } from 'styles/formation-theme'
-import GlobalStyle from 'styles/global'
+import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [darkMode, setDarkMode] = useState(false)
+
+  // Check window and document existentce to limit logic to the client-side
+  if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+    // https://tailwindcss.com/docs/dark-mode#supporting-system-preference-and-manual-selection
+    if (
+      darkMode === true ||
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+    ) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }
+
   return (
-    <Grommet themeMode={darkMode ? 'dark' : 'light'} theme={theme} full>
-      <Row gap="4px" pad="small" justify="start">
-        <Col
-          border={{ size: 'small', color: 'black' }}
-          background="black"
-          width="24px"
-          height="24px"
-          round="12px"
-          onClick={() => setDarkMode(true)}
-        />
-        <Col
-          border={{ color: 'brand', size: 'small' }}
-          background="white"
-          width="24px"
-          height="24px"
-          round="12px"
-          onClick={() => setDarkMode(false)}
-        />
-      </Row>
-      <GlobalStyle />
-      <Col height={{ min: '100%' }}>
-        <Component {...pageProps} />
-      </Col>
-    </Grommet>
+    <div className="bg-white dark:bg-black dark:text-white">
+      <Component {...pageProps} />
+    </div>
   )
 }
 
