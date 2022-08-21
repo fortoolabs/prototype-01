f3529f2bbf43f8f1a33bf5fdc0f4e16c279b1db7
David Asabina <vid@bina.me>
2022-08-21 17:31:42 +0000

    feat: add dark-mode toggle to top view

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,16 @@ import { useState } from 'react'
 
 import '../styles/globals.css'
 
+import ToggleDarkMode from 'components/ToggleDarkMode'
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const [darkMode, setDarkMode] = useState(false)
+  const [isDark, setDarkMode] = useState(false)
 
   // Check window and document existentce to limit logic to the client-side
   if (typeof window !== 'undefined' && typeof document !== 'undefined') {
     // https://tailwindcss.com/docs/dark-mode#supporting-system-preference-and-manual-selection
     if (
-      darkMode === true ||
+      isDark === true ||
       window.matchMedia('(prefers-color-scheme: dark)').matches
     ) {
       document.documentElement.classList.add('dark')
@@ -22,6 +24,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <div className="bg-white dark:bg-black dark:text-white">
+      <ToggleDarkMode isDark={isDark} setDarkMode={setDarkMode} />
       <Component {...pageProps} />
     </div>
   )
