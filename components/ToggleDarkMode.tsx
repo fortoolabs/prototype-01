551f30353bbd4c28f4e086345611d5ff2eedd508
David Asabina <vid@bina.me>
2022-08-21 17:36:52 +0000

    fix: rename properties for JSX compat

--- components/ToggleDarkMode.tsx
+++ components/ToggleDarkMode.tsx
@@ -43,15 +43,15 @@ export default function ToggleDarkMode({
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-3 w-3 text-gray-400"
+            className="h-3 w-3 text-gray-400"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            stroke-width="2"
+            strokeWidth="2"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
             />
           </svg>
