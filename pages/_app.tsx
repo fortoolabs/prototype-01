ea2191aa02b4a7eaa6fcd4d2c0e1071069410fe8
David Asabina <vid@bina.me>
2022-08-21 17:55:23 +0000

    chore: move window/document effects into hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import '../styles/globals.css'
 
@@ -9,18 +9,20 @@ import ToggleDarkMode from 'components/ToggleDarkMode'
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [isDark, setDarkMode] = useState(false)
 
-  // Check window and document existentce to limit logic to the client-side
-  if (typeof window !== 'undefined' && typeof document !== 'undefined') {
-    // https://tailwindcss.com/docs/dark-mode#supporting-system-preference-and-manual-selection
-    if (
-      isDark === true ||
-      window.matchMedia('(prefers-color-scheme: dark)').matches
-    ) {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
+  useEffect(() => {
+    // Check window and document existentce to limit logic to the client-side
+    if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+      // https://tailwindcss.com/docs/dark-mode#supporting-system-preference-and-manual-selection
+      if (
+        isDark === true ||
+        window.matchMedia('(prefers-color-scheme: dark)').matches
+      ) {
+        document.documentElement.classList.add('dark')
+      } else {
+        document.documentElement.classList.remove('dark')
+      }
     }
-  }
+  })
 
   return (
     <div className="bg-white dark:bg-black dark:text-white">
