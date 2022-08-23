6d4c98b1378eba347bede0880c3ba8448a6e75aa
David Asabina <vid@bina.me>
2022-08-23 11:14:21 +0000

    chore: update Dialog.Panel to match template

--- components/KanbanBoard.tsx
+++ components/KanbanBoard.tsx
@@ -668,50 +668,96 @@ export const KanbanEditTaskModal = React.forwardRef(EditTaskModal)
 type AddTaskModalProps = {
   show: () => any
   hide: () => any
+  submit: () => any
 }
 
 // FIX: Type ref as RefObject-like
-const AddTaskModal = ({ show, hide }: AddTaskModalProps, ref: any) => {
+const AddTaskModal = ({ show, hide, submit }: AddTaskModalProps, ref: any) => {
+  const onSubmit = (e) => {
+    e.preventDefault()
+    submit()
+  }
+
   return (
-    <Dialog.Panel className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
-      <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-        <div className="sm:flex sm:items-start">
-          <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-            <XIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
-          </div>
-          <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-            <Dialog.Title
-              as="h3"
-              className="text-lg leading-6 font-medium text-gray-900"
-            >
-              Deactivate account
-            </Dialog.Title>
-            <div className="mt-2">
-              <p className="text-sm text-gray-500">
-                Are you sure you want to deactivate your account? All of your
-                data will be permanently removed. This action cannot be undone.
-              </p>
+    <Dialog.Panel className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all max-w-2xl sm:my-8 sm:max-w-xl sm:w-full">
+      <div className="flex items-center justify-between p-4 border-b rounded-t md:px-6 dark:border-gray-700">
+        <Dialog.Title
+          as="div"
+          className="text-xl font-semibold dark:text-white"
+        >
+          Add new task
+        </Dialog.Title>
+        <button
+          type="button"
+          className="text-gray-400 bg-transparent hover:bg-gray-300 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-700 dark:hover:text-white"
+          ref={ref}
+          onClick={() => hide()}
+        >
+          <XIcon className="w-5 h-5" />
+        </button>
+      </div>
+      <form onSubmit={onSubmit}>
+        <div className="p-4 space-y-6 md:px-6">
+          <div className="grid grid-cols-2 gap-6 mb-4">
+            <div className="col-span-6 sm:col-span-3">
+              <label
+                htmlFor="product-name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Task Name
+              </label>
+              <input
+                type="text"
+                name="product-name"
+                id="product-name"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:placeholder-gray-400"
+                placeholder="Redesign Homepage"
+                required
+              />
+            </div>
+            <div className="col-span-6 sm:col-span-3">
+              <label
+                htmlFor="product-details"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Enter a description
+              </label>
+              <textarea
+                id="product-details"
+                rows={6}
+                className="block w-full text-gray-900 border border-gray-200 rounded-lg bg-gray-50 sm:text-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:placeholder-gray-400"
+                placeholder="On line 672 you define $table_variants. Each instance of 'color-level' needs to be changed to 'shift-color'."
+              ></textarea>
             </div>
           </div>
+          <div className="flex items-center justify-center w-full">
+            <label className="flex items-center justify-center w-full h-32 text-gray-500 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer hover:bg-gray-100 hover:border-gray-300 hover:text-gray-900 dark:border-gray-700 dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:hover:text-white">
+              <div className="flex items-center justify-center space-x-2">
+                <PhotographIcon className="w-8 h-8" stroke="currentColor" />
+                <p className="text-base">Drop files to upload</p>
+              </div>
+              <input type="file" className="hidden" />
+            </label>
+          </div>
         </div>
-      </div>
-      <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-        <button
-          type="button"
-          className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-          onClick={() => setAdd(false)}
-        >
-          Deactivate
-        </button>
-        <button
-          type="button"
-          className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-          onClick={() => setAdd(false)}
-          ref={ref}
-        >
-          Cancel
-        </button>
-      </div>
+        <div className="flex items-center p-4 space-x-3 border-t border-gray-200 rounded-b md:p-6 dark:border-gray-700">
+          <button
+            type="submit"
+            className="w-32 inline-flex items-center justify-center text-white bg-blue-700 hover:bg-blue-800 border border-blue-700 hover:border-blue-800 font-semibold rounded-lg text-sm py-2.5 text-center"
+          >
+            <SolidPlusIcon className="w-5 h-5 mr-2" />
+            Add Card
+          </button>
+          <button
+            type="button"
+            ref={ref}
+            onClick={() => hide()}
+            className="w-24 text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 hover:border-gray-300 font-semibold rounded-lg text-sm py-2.5 text-center dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:bg-gray-600"
+          >
+            Close
+          </button>
+        </div>
+      </form>
     </Dialog.Panel>
   )
 }
@@ -764,6 +810,7 @@ export default function KanbanSpace({ data }: KanbanSpaceProps) {
                 <KanbanAddTaskModal
                   show={() => setAdd(true)}
                   hide={() => setAdd(false)}
+                  submit={() => console.log('submitting')}
                   ref={cancelButtonRef}
                 />
               </Transition.Child>
