2c4ad12a70a4f2ebd2dd0431d39ebae3e4c7580b
David Asabina <vid@bina.me>
2022-08-23 17:03:10 +0000

    refactor: use subcomponent within TOC to compose hierarchy

--- components/app/TOC.tsx
+++ components/app/TOC.tsx
@@ -9,18 +9,24 @@ export type TOCProps = {
   headings: FNestedTableOfContents
 }
 
-// TODO: Refactor into React Component and key mapped elements
 // TODO: Use Disclosure to support folding
-const recurList = (
-  { plaintext, children }: FNestedTableOfContentsEntry,
-  depth: number,
-) => {
+type TableOfContentsEntryProps = {
+  entry: FNestedTableOfContentsEntry
+  depth: number
+}
+
+function TableOfContentsEntry({
+  entry: { children, plaintext },
+  depth,
+}: TableOfContentsEntryProps) {
   return (
     <li className="py-2 w-full">
       <a href="#test-anchor">{plaintext}</a>
       {children !== [] && (
         <ul className={`px-${2 * depth}`}>
-          {children.map((child) => recurList(child, depth + 1))}
+          {children.map((heading, idx) => (
+            <TableOfContentsEntry key={idx} entry={heading} depth={depth + 1} />
+          ))}
         </ul>
       )}
     </li>
@@ -31,9 +37,9 @@ export default function TOC({ headings }: TOCProps) {
   if (!headings.length) return null
   return (
     <ul className="text-sm font-medium text-gray-700 group-hover:text-gray-900">
-      {headings.map((item) => {
-        return recurList(item, 1)
-      })}
+      {headings.map((heading, idx) => (
+        <TableOfContentsEntry key={idx} entry={heading} depth={1} />
+      ))}
     </ul>
   )
 }
