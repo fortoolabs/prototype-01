e3e324df4382c107bc127bdb6f3feaaff30c08a0
David Asabina <vid@bina.me>
2022-08-26 15:54:07 +0000

    fix: complete vitest config for in-source testing
    as per https://vitest.dev/guide/in-source.html

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,8 +7,11 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  define: {
+    'import.meta.vitest': 'undefined',
+  },
   test: {
     environment: 'jsdom',
-    includeSource: ['components/**/*.{js,jsx,ts,tsx}']
+    includeSource: ['components/**/*.{js,jsx,ts,tsx}'],
   },
 })
