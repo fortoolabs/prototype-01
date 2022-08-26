e73def62eb6497cc411758a42803cea491a4216d
David Asabina <vid@bina.me>
2022-08-26 13:56:56 +0000

    chore: stub Kanban data extraction func + related test

--- components/Board/index.tsx
+++ components/Board/index.tsx
@@ -9,6 +9,55 @@ import KanbanEditTaskModal from './EditTaskModal'
 // TODO: Remove when addressing the next (state) TODO
 import { columnsFromBackend } from './data'
 
+import { v4 as uuidv4 } from 'uuid'
+import parse from 'core/parser'
+
+// Extraction of Kanban data is component-specific so it belongs in the component implementation
+function extractKanbanData(doc: FDocument) {
+  return {}
+}
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest
+
+  it('extracts columns structure from a listing of todos', () => {
+    const x = extractKanbanData(
+      parse(
+        [
+          '#+TITLE: Just a test doc',
+          '* Background',
+          '* Research',
+          '** TODO Collect information',
+          '* Implementation',
+          '** Build prototype',
+        ].join('\n'),
+      ),
+    )
+    expect(x).toMatchInlineSnapshot(`
+      {
+        "DONE": {
+          "tasks": [],
+          "title": "DONE",
+        },
+        "TODO": {
+          "tasks": [
+            {
+              "columnId": "TODO",
+              "completed": false,
+              "daysLeft": 0,
+              "description": "",
+              "id": "research-collect-information",
+              "members": [],
+              "name": "Collect information",
+            },
+          ],
+          "title": "TODO",
+        },
+      }
+    `)
+  })
+}
+
 export default function KanbanSpace({ doc }: { doc: FDocument }) {
   const [isEdit, setEdit] = useState(false)
   const [isAdd, setAdd] = useState(false)
