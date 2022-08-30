b0f9d98d273808813c26ea3aedd27fcc31f25345
David Asabina <vid@bina.me>
2022-08-30 21:21:28 +0000

    chore: more or less dump logo from Inkscape

---
+++ components/app/Logo.tsx
@@ -0,0 +1,38 @@
+export default function LogoIcon() {
+  return (
+    <svg
+      width="100"
+      height="100"
+      viewBox="0 0 26.458333 26.458333"
+      version="1.1"
+      id="svg5"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs id="defs2" />
+      <rect
+        style="fill:currentColor"
+        id="rect286"
+        width="10.583333"
+        height="5.2916665"
+        x="11.90625"
+        y="3.96875"
+      />
+      <rect
+        style="fill:currentColor"
+        id="rect288"
+        width="5.2916665"
+        height="10.583333"
+        x="3.96875"
+        y="11.90625"
+      />
+      <rect
+        style="fill:currentColor"
+        id="rect450"
+        width="5.2916665"
+        height="5.2916665"
+        x="11.90625"
+        y="11.90625"
+      />
+    </svg>
+  )
+}
