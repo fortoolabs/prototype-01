db878c2184242b6dcbee5b9519733227d057dc2e
David Asabina <vid@bina.me>
2022-08-30 22:10:09 +0000

    fix: cleanup Logo coordinates

--- components/app/Logo.tsx
+++ components/app/Logo.tsx
@@ -1,33 +1,35 @@
 export default function LogoIcon() {
   return (
     <svg
-      width="100"
-      height="100"
-      viewBox="0 0 26.458333 26.458333"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
       version="1.1"
-      id="svg5"
       xmlns="http://www.w3.org/2000/svg"
     >
       <rect
-        fill="none"
-        width="10.583333"
-        height="5.2916665"
-        x="11.90625"
-        y="3.96875"
+        fill="currentColor"
+        id="logo-flank-top"
+        width="14"
+        height="7"
+        x="10"
+        y="0"
       />
       <rect
-        fill="none"
-        width="5.2916665"
-        height="10.583333"
-        x="3.96875"
-        y="11.90625"
+        fill="currentColor"
+        id="logo-flank-side"
+        width="7"
+        height="14"
+        x="0"
+        y="10"
       />
       <rect
-        fill="none"
-        width="5.2916665"
-        height="5.2916665"
-        x="11.90625"
-        y="11.90625"
+        fill="currentColor"
+        id="logo-core"
+        width="7"
+        height="7"
+        x="10"
+        y="10"
       />
     </svg>
   )
