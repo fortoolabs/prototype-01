419669d156c5918ad37b35b195a39c33ef881881
hiiiP0wer <tijan@assutech.gm>
2022-09-02 15:02:41 +0000

    implement Date component and example stories

--- components/Date.tsx
+++ components/Date.tsx
@@ -1,25 +1,64 @@
-import { Calendar } from 'grommet-icons'
-
-import { Row } from 'components/View'
-
-import { formatDate } from 'utils/time'
+import { CalendarIcon, ClockIcon } from '@heroicons/react/20/solid'
+import { formatDate, formatTime } from 'utils/time'
 
 export type DateProps = {
   timestamp: number
+  as: 'time' | 'date'
+  border?: boolean
+  iconFill?: 'blue' | 'green' | 'indigo' | 'purple' | 'gray'
 }
 
-export default function Date({ timestamp }: DateProps) {
+const fill = {
+  gray: 'fill-gray-600',
+  blue: 'fill-blue-600',
+  green: 'fill-green-600',
+  indigo: 'fill-indigo-600',
+  purple: 'fill-purple-600',
+}
+
+export default function Date({ timestamp, as, border, iconFill }: DateProps) {
   return (
-    <Row
-      round="xlarge"
-      border={{ size: 'small' }}
-      pad={{ horizontal: 'medium', vertical: 'small' }}
-      gap="medium"
+    <time
+      className={[
+        'inline-flex',
+        'items-baseline',
+        'gap-1',
+        'font-semibold',
+        'bg-gray-200',
+        border ? 'border border-gray-500' : '',
+        'py-[2px]',
+        'px-[3px]',
+        'rounded-md',
+        'text-sm',
+      ].join(' ')}
+      dateTime={
+        as === 'time'
+          ? formatTime(timestamp).datetime
+          : formatDate(timestamp).datetime
+      }
     >
-      <Calendar color="brand" />
-      <span>
-        <b>{formatDate(timestamp)}</b>
-      </span>
-    </Row>
+      {as === 'date' && (
+        <CalendarIcon
+          className={[
+            'self-center',
+            'w-5',
+            'h-5',
+            iconFill ? fill[iconFill] : fill['blue'],
+          ].join(' ')}
+        />
+      )}
+      {as === 'time' && (
+        <ClockIcon
+          className={[
+            'self-center',
+            'w-5',
+            'h-5',
+            iconFill ? fill[iconFill] : fill['blue'],
+          ].join(' ')}
+        />
+      )}
+      {as === 'date' && formatDate(timestamp).date}
+      {as === 'time' && formatTime(timestamp).time}
+    </time>
   )
 }
