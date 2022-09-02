29cbd6ca75c2ea6f8753cc63c7eb0443d95ef73a
David Asabina <vid@bina.me>
2022-09-02 10:06:08 +0000

    chore: tighten Linear doc input + drop Grommet + show empty state

--- components/Linear/index.tsx
+++ components/Linear/index.tsx
@@ -1,32 +1,49 @@
-import { Row } from 'components/View'
-import { Main } from 'components/View'
-
 import { renderElement } from 'core/renderer'
 
 import { FDocument } from 'core/types'
 
+import { LightBulbIcon } from '@heroicons/react/24/outline'
+
 export type LinearProps = {
   // TODO: Revert to make this mandatory, optional doc is a hack to move this forward
-  doc?: FDocument
+  doc: FDocument
   // TODO: Remove in preference of theming (as outlined below)
   serif?: boolean
 }
 
 export default function Linear({ serif, doc }: LinearProps) {
-  // TODO: Use theming context/provider for this or define own
+  // TODO: Explore using a theming context/provider
   // https://reactician.com/articles/sharing-state-between-nextjs-page-navigations-using-react-contexts
-  if (doc === undefined) {
-    // TODO: Implement empty board view
-    return <span>noop</span>
+  const { content, title } = doc
+
+  if (content.length === 0) {
+    return (
+      <button
+        type="button"
+        className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      >
+        <LightBulbIcon
+          className="mx-auto h-12 w-12 text-gray-400 stroke-current stroke-1"
+          aria-hidden="true"
+        />
+        <span className="mt-2 block text-sm font-medium text-gray-900">
+          Start forming this clean slate with fresh ideas!
+        </span>
+      </button>
+    )
   }
 
-  const { content } = doc
   return (
-    <Row gap="small" pad="small" alignSelf="center">
-      <Main style={{ fontFamily: serif ? 'Times' : 'inherit' }}>
-        {/* iterate over json, build right component */}
-        {content.flatMap((el, i) => renderElement(el, i))}
-      </Main>
-    </Row>
+    <div
+      className={[
+        serif ? 'font-serif' : 'font-sans',
+        //'font-mono',
+        'text-base max-w-prose',
+      ].join('')}
+    >
+      {content.length} {title}
+      {/* iterate over json, build right component */}
+      {content.flatMap((el, i) => renderElement(el, i))}
+    </div>
   )
 }
