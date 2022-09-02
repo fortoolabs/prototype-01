24e43b5d7f4cef5eb82b8d4200fe650c9df65b81
David Asabina <vid@bina.me>
2022-09-02 15:02:04 +0000

    chore: stub Linear/Breadcrumb

--- components/Linear/index.tsx
+++ components/Linear/index.tsx
@@ -15,40 +15,35 @@ type BreadcrumbsProp = {
 }
 function Breadcrumbs({ pages }: BreadcrumbsProp) {
   return (
-    <nav className="flex" aria-label="Breadcrumb">
+    <nav aria-label="Breadcrumb">
       <ol
         role="list"
-        className="flex w-full sm:w-auto space-x-4 rounded-md bg-white px-6 shadow"
+        className="flex h-8 w-full sm:w-auto space-x-4 rounded-md bg-white px-6 shadow"
       >
-        <li className="flex">
-          <div className="flex items-center">
-            <a href="#" className="text-gray-400 hover:text-gray-500">
-              <HomeIcon className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
-              <span className="sr-only">Home</span>
-            </a>
-          </div>
+        <li className="flex items-center">
+          <a href="#" className="text-gray-400 hover:text-gray-500">
+            <HomeIcon className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
+            <span className="sr-only">Home</span>
+          </a>
         </li>
         {pages.map((page) => (
-          <li key={page.name} className="flex">
-            <div className="flex items-center">
-              <svg
-                className="h-full w-6 flex-shrink-0 text-gray-200"
-                viewBox="0 0 24 44"
-                preserveAspectRatio="none"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-              >
-                <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
-              </svg>
-              <a
-                href={page.target}
-                className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
-                aria-current={page.current ? 'page' : undefined}
-              >
-                {page.name}
-              </a>
-            </div>
+          <li key={page.name} className="flex items-center overflow-hidden">
+            <svg
+              className="h-5 w-5 flex-shrink-0 text-gray-300"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              aria-hidden="true"
+            >
+              <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+            </svg>
+            <a
+              href={page.target}
+              className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700 truncate"
+              aria-current={page.current ? 'page' : undefined}
+            >
+              {page.name}
+            </a>
           </li>
         ))}
       </ol>
