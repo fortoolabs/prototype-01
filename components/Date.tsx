bebb77910da81fb195394b99c0cde5c5ec5d49a0
David Asabina <vid@bina.me>
2022-09-04 11:23:40 +0000

    doc: address DRY-concern in TODO

--- components/Date.tsx
+++ components/Date.tsx
@@ -21,6 +21,22 @@ export default function Date({ timestamp, as, iconFill }: DateProps) {
     'aria-hidden': true,
   }
 
+  // TODO: @tijan let's spar on how we can share common styles between
+  // components.
+  // During merge, I tweaked this component to be more in line with
+  // the styling of the Link and Fallback components and then realized that
+  // we're repeating outselves quite a bit to get the classNames right for the
+  // different components.
+  // Things like backgrounds, drop-shadows, paddings and justification/alignment
+  // should be shared across all components such that we can change the
+  // look-and-feel in one place and see it applied uniformly. We therefore need
+  // to think of an architecture/design that is more scalable.
+  // Let's spar about this or feel free to propose design candidates. According
+  // to https://tailwindcss.com/docs/reusing-styles, my undestanding is that
+  // abstracting the shared styling into a separate components, perhaps
+  // InlineElement and BlockElement, will allow all of our components to share
+  // some common styling by composing/wrapping them as
+  // <InlineElement>{children}</InlineElement>.
   return (
     <time
       className={[
