aab3f2ce5fbfba8dfd7c72a4e6b6aac0591b96d8
David Asabina <vid@bina.me>
2022-09-07 19:11:58 +0000

    chore: typeset headings bold

--- components/doc/Heading.tsx
+++ components/doc/Heading.tsx
@@ -12,30 +12,30 @@ function Heading({ children, level }: PropsWithChildren<HeadingProps>) {
   switch (level) {
     case '1':
     case 1:
-      return <h1>{children}</h1>
+      return <h1 className="font-bold text-5xl">{children}</h1>
 
     case '2':
     case 2:
-      return <h2>{children}</h2>
+      return <h2 className="font-bold text-3xl">{children}</h2>
 
     case '3':
     case 3:
-      return <h3>{children}</h3>
+      return <h3 className="font-bold text-2xl">{children}</h3>
 
     case '4':
     case 4:
-      return <h4>{children}</h4>
+      return <h4 className="font-bold text-xl">{children}</h4>
 
     case '5':
     case 5:
-      return <h5>{children}</h5>
+      return <h5 className="font-bold text-lg">{children}</h5>
 
     case '6':
     case 6:
-      return <h6>{children}</h6>
+      return <h6 className="font-bold text-2xl">{children}</h6>
 
     default:
-      return <p className={`heading-${level}`}>{children}</p>
+      return <p className={`font-bold heading-${level}`}>{children}</p>
   }
 }
 
