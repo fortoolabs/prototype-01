7f6a1753a27364b78fab198bc7f548fb540de535
David Asabina <vid@bina.me>
2022-09-09 13:08:54 +0000

    chore: expose encodeTarget helper

--- core/helpers.tsx
+++ core/helpers.tsx
@@ -1,5 +1,9 @@
 import base64url from 'base64url'
 
-export const defaultTarget = base64url.encode(
+export const defaultTarget = encodeTarget(
   'https://gitlab.com/formation.tools/eng/engineering/-/raw/main/README.org',
 )
+
+export function encodeTarget(url: string) {
+  return base64url.encode(url)
+}
