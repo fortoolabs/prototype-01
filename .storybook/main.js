e6ee099d3b31a3fc12c6abf31347bdb5e085cfdd
David Asabina <vid@bina.me>
2022-09-12 17:17:11 +0000

    fix: remove ./stories/ from Storybook paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,8 +5,6 @@ module.exports = {
   stories: [
     '../components/*.stories.@(js|jsx|ts|tsx)',
     '../components/**/*.stories.@(js|jsx|ts|tsx)',
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
