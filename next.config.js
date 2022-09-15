55c656e914444c83dc9725e69ed08510e4f7b429
Cyborg <89103307+jambaceesay@users.noreply.github.com>
2022-09-15 16:33:48 +0000

    add rewrites for palusible  & move script  to root

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+
+  async rewrites() {
+    return [
+        {
+            source: '/js/script.js',
+            destination: 'https://plausible.io/js/script.js'
+        },
+        {
+            source: '/api/event',
+            destination: 'https://plausible.io/api/event'
+        }
+    ];
+},
 }
 
 module.exports = nextConfig
