d1b2a8b2e76ccb35519411b25292fc8b1e3c5379
552020 <stefanolombardo@posteo.de>
2022-10-26 15:45:25 +0200

    add Secondary Avatar

--- components/app/Menu.stories.tsx
+++ components/app/Menu.stories.tsx
@@ -16,10 +16,18 @@ export default {
 const Template: ComponentStory<typeof Avatar> = (args) => <Avatar {...args} />
 
 export const Primary = Template.bind({})
+export const Secondary = Template.bind({})
 
 Primary.args = {
   name: 'David',
   handle: 'vidbina',
   avatarPath:
     'https://pbs.twimg.com/profile_images/1276458607702241282/eAH3B2eT_400x400.jpg',
+}
+
+Secondary.args = {
+  name: 'Lexi',
+  handle: 'lfridman',
+  avatarPath:
+    'https://pbs.twimg.com/profile_images/956331551435960322/OaqR8pAB_400x400.jpg',
 }
