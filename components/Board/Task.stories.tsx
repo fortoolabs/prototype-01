788312bbd46e2b12b427fdc657f7dc77015bf342
552020 <stefanolombardo@posteo.de>
2022-11-14 19:13:16 +0100

    first try

--- components/Board/Task.stories.tsx
+++ components/Board/Task.stories.tsx
@@ -15,3 +15,49 @@ const Template: ComponentStory<typeof KanbanTask> = (args) => (
 
 export const Primary = Template.bind({})
 export const Secondary = Template.bind({})
+export const Completed = Template.bind({})
+
+Primary.args = {
+  index: 123,
+  task: {
+    id: '456',
+    columnId: '678',
+    name: 'This Task',
+    description: 'Accurate but concise description',
+    completed: false,
+    daysLeft: 5,
+    tags: ['storybook', 'busywork'],
+    members: [{ id: 456, name: 'David', avatar: 'this url' }],
+  },
+  onEditTask: 'whatever',
+}
+
+Primary.args = {
+  index: 789,
+  task: {
+    id: '111',
+    columnId: '222',
+    name: 'Second Task',
+    description: 'An other description',
+    completed: false,
+    daysLeft: 5,
+    tags: ['storybook', 'busywork'],
+    members: [{ id: 456, name: 'David', avatar: 'this url' }],
+  },
+  onEditTask: 'whatever',
+}
+
+Completed.args = {
+  index: 123,
+  task: {
+    id: '456',
+    columnId: '678',
+    name: 'This Task',
+    description: 'Accurate but concise description',
+    completed: true,
+    daysLeft: 5,
+    tags: ['storybook', 'busywork'],
+    members: [{ id: 456, name: 'David', avatar: 'this url' }],
+  },
+  onEditTask: 'whatever',
+}
