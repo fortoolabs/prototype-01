f61b5aba446bde42c89a238a9fbed152c21dd402
David Asabina <vid@bina.me>
2022-12-06 07:35:53 +0000

    chore: reorg and reduce by removing comments (unused code)

--- components/doc/List.tsx
+++ components/doc/List.tsx
@@ -7,10 +7,11 @@ import {
   HTMLAttributes,
 } from 'react'
 
+import { Disclosure } from '@headlessui/react'
+
 import { ChevronUpIcon } from '@heroicons/react/20/solid'
 
 import { blockClasses } from 'components/doc/Block'
-import { Disclosure } from '@headlessui/react'
 
 export type ListWithClassNames = HTMLAttributes<'ol'> | HTMLAttributes<'ul'>
 export type ListProps = ListWithClassNames
@@ -33,41 +34,6 @@ export function ListChild({
   label,
   children,
 }: PropsWithChildren<ListChildProps>) {
-  /* stefano commented this out cause they are never used - their purpose was to implement Discslosure manually
-
-  const [showChildren, setShowChildren] = useState(true)
-
-  const animation =
-    '[&>div]:overflow-hidden [&>div]:transition-[max-height] [&>div]:duration-100 [&>div]:ease-in-out'
-
-  const itemClasses = [
-    'relative h-fit',
-    showChildren
-      ? `[&>div]:max-h-screen ${animation}`
-      : `[&>div]:max-h-0 ${animation} [&>p]:hidden [&>p:first-child]:block`,
-    // children ? 'first:list-none' : 'list-disc',
-  ].join(' ')
-
-  const handleClick = () => {
-    setShowChildren(!showChildren)
-  }
-  */
-
-  // TODO: @tijan let's discuss whether we use Disclosure here or not.
-  // The div-transition magic is too complicated for my brain to quickly debug. 😅
-  //return (
-  //  <Disclosure>
-  //    <Disclosure.Button className="py-2">
-  //      Is team pricing available?
-  //    </Disclosure.Button>
-  //    <Disclosure.Panel className="text-gray-500">
-  //      Yes! You can purchase a license that you can share with your entire
-  //      team.
-  //    </Disclosure.Panel>
-  //  </Disclosure>
-  //)
-
-  // const hasChildren = children?.length > 0
   const hasChildren = React.Children.count(children) > 0
 
   return (
@@ -89,30 +55,6 @@ export function ListChild({
           </>
         )}
       </Disclosure>
-
-      {/* <li className={itemClasses}>
-        {label}
-        {Children.count(children) > 0 && (
-          <div className="">
-            {children}
-            {false && (
-              <ChevronUpIcon
-                className={[
-                  'absolute',
-                  'top-0',
-                  'h-5',
-                  'w-5',
-                  'cursor-pointer',
-                  'select-none',
-                  'transition',
-                  showChildren ? '' : 'rotate-180 transition duration-100',
-                ].join(' ')}
-                onClick={handleClick}
-              />
-            )}
-          </div>
-        )}
-      </li> */}
     </>
   )
 }
